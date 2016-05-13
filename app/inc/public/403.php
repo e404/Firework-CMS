bae1eaaf0f8cefbdef01752f88ba7d53d9e0357a
Gerald Schittenhelm <gerald@schittenhelm.at>
2016-05-13 18:41:11 +0200

    Skinned error files (403, 404)

--- app/inc/public/403.php
+++ app/inc/public/403.php
@@ -1,6 +1,15 @@
 <?php
 
+require_once('../global.php');
+
 header('HTTP/1.1 403 Forbidden');
+
+$skinfile = App::getSkinPath().'+403.php';
+if(file_exists($skinfile)) {
+	include($skinfile);
+	App::halt();
+}
+
 echo '<html><head><style>body{background:#555;color:#fff;font-family:sans-serif;text-align:center;margin:3em;}</style></head><body>';
 echo '<h1>Access Denied</h1><p>You don\'t have permission to access '.htmlspecialchars($_SERVER['REQUEST_URI']).' on this server.</p>';
 echo '</body></html>';
--- app/inc/public/404.php
+++ app/inc/public/404.php
@@ -1,6 +1,15 @@
 <?php
 
+require_once('../global.php');
+
 header('HTTP/1.1 404 Not Found');
+
+$skinfile = App::getSkinPath().'+404.php';
+if(file_exists($skinfile)) {
+	include($skinfile);
+	App::halt();
+}
+
 echo '<html><head><style>body{background:#555;color:#fff;font-family:sans-serif;text-align:center;margin:3em;}</style></head><body>';
 echo '<h1>Not Found</h1><p>The requested URI '.htmlspecialchars($_SERVER['REQUEST_URI']).' was not found on this server.</p>';
 echo '</body></html>';
