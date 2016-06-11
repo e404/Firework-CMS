5a72d354836c534a8ef286a4701f8b76fc44cd40
Gerald Schittenhelm <gerald@schittenhelm.at>
2016-06-11 19:02:44 +0200

    added Button::disabled()

--- app/cls/HtmlElements/Button.php
+++ app/cls/HtmlElements/Button.php
@@ -6,10 +6,17 @@
 class Button extends AbstractFormField {
 
 	protected $action = '';
+	protected $disabled = false;
 
 	protected function init() {
 		$this->setType('button');
 		$this->setCssClass('button');
+	}
+
+	public function disabled($disabled=null) {
+		if($disabled===null) return $this->disabled;
+		$this->disabled = (bool) $disabled;
+		return $this;
 	}
 
 	public function setAction($action) {
@@ -20,6 +27,9 @@ class Button extends AbstractFormField {
 		$html = $this->getGenericHtml();
 		if($this->action) {
 			$html = str_replace('<input ','<input onclick="'.$action.'" ',$html);
+		}
+		if($this->disabled) {
+			$html = str_replace('<input ','<input disabled="disabled" ',$html);
 		}
 		return parent::getHtml($html);
 	}
