58ed21e169d56aaa8b2a4d948f979f4e41845267
Gerald Schittenhelm <gerald@schittenhelm.at>
2016-06-19 21:13:03 +0200

    fixed email html to text links

--- app/cls/System/EMail.php
+++ app/cls/System/EMail.php
@@ -298,9 +298,14 @@ class EMail extends Instantiable {
 		$text = preg_replace("@<br[^>]*?>@si","[\0]",$text);
 		$text = preg_replace("@</div>@i","[\0]",$text);
 		$text = preg_replace("@</(p|h1|h2|h3|h4|h5|h6)>@i","[\0][\0]",$text);
-		$text = preg_replace_callback('@<a(\s+[^>]+)>@si', function($matches){
-			if(!preg_match('@\shref=["\']([^"\']+)["\']@', $matches[1], $href)) return '';
-			return '[=URL'.$href[1].'=URL] ';
+		$text = preg_replace_callback('@<a(\s+[^>]+)>(.*?)</a>@si', function($link){
+			if(!preg_match('@\shref=["\']([^"\']+)["\']@', $link[1], $href)) return '';
+			$url = $href[1];
+			$text = trim($link[2]);
+			$url_normalized = strtolower($url);
+			$text_normalized = strtolower($text);
+			if($url_normalized===$text_normalized || strstr($url_normalized, $text_normalized)) return $url;
+			return $text.' [=URL'.$url.'=URL]';
 		},$text);
 		$text = preg_replace("@<[\/\!]*?[^<>]*?>@si","",$text);
 		$text = preg_replace("@<![\s\S]*?--[ \t\n\r]*>@","",$text);
