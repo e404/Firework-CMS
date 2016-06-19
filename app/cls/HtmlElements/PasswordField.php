3a33caaeac96e91c25e043c0bccd4084d95b391f
Gerald Schittenhelm <gerald@schittenhelm.at>
2016-06-19 15:04:55 +0200

    fixed password field passhash form check

--- app/cls/HtmlElements/PasswordField.php
+++ app/cls/HtmlElements/PasswordField.php
@@ -5,36 +5,20 @@
  */
 class PasswordField extends AbstractFormField {
 
-	protected function isPassHash($str) {
-		return !!preg_match('/^[0-9a-f]{64}$/',$str);
-	}
-
-	protected function makePassHash($str) {
-		for($i=0; $i<1000; $i++) {
-			$str = hash('sha256',$str);
-		}
-		return $str;
-	}
-
-	public function getPassHash() {
-		$value = parent::getUserValue();
-		if(!$value) return null;
-		if($this->isPassHash($value)) return $value;
-		return $this->makePassHash($value);
-	}
-
 	protected function init() {
 		$this->setType('password');
 		$this->setCssClass('text');
 	}
 
-	protected function getAutofillValue() {
-		return $this->autofill ? $this->getPassHash() : null;
-	}
-
-	public function getHtml() {
-		$hash = $this->getAutofillValue();
-		return parent::getHtml('<label><span>'.$this->getLabelHtml().' </span><input type="'.$this->type.'" name="'.$this->name.'" value="'.htmlspecialchars($hash).'"></label>');
+	public function getPassHash() {
+		$value = parent::getUserValue();
+		if(!$value) return null;
+		if(!preg_match('/^[0-9a-f]{64}$/',$value)) {
+			for($i=0; $i<1000; $i++) {
+				$value = hash('sha256',$value);
+			}
+		}
+		return $value;
 	}
 
 }
