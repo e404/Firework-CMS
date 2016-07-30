7d4e19cdae3170d6299ae2acef00e8c799af7d8e
Gerald Schittenhelm <gerald@schittenhelm.at>
2016-07-30 11:43:25 +0800

    added TextField::setPlaceholder()

--- app/cls/HtmlElements/TextField.php
+++ app/cls/HtmlElements/TextField.php
@@ -7,6 +7,7 @@ class TextField extends AbstractFormField {
 
 	protected $multiline = false;
 	protected $jsautocorrect = null;
+	protected $placeholder = null;
 
 	protected function init() {
 		$this->setType('text');
@@ -23,6 +24,10 @@ class TextField extends AbstractFormField {
 		return $this;
 	}
 
+	public function setPlaceholder($placeholder_text) {
+		$this->placeholder = $placeholder_text;
+	}
+
 	public function getHtml() {
 		$userValue = $this->getAutofillValue();
 		$html = '<label><span>'.$this->getLabelHtml().' </span>';
@@ -31,9 +36,9 @@ class TextField extends AbstractFormField {
 			$onchange = ' onchange="this.value=('.str_replace('"','&quot;',$this->jsautocorrect).')(this.value); return false;"';
 		}
 		if($this->multiline) {
-			$html.= '<textarea name="'.$this->name.'"'.($this->maxlength ? ' maxlength="'.$this->maxlength.'"' : '').$onchange.'>'.htmlspecialchars($userValue===null ? $this->value : $userValue).'</textarea>';
+			$html.= '<textarea name="'.$this->name.'"'.($this->maxlength ? ' maxlength="'.$this->maxlength.'"' : '').($this->placeholder ? ' placeholder="'.htmlspecialchars($this->placeholder).'"' : '').$onchange.'>'.htmlspecialchars($userValue===null ? $this->value : $userValue).'</textarea>';
 		}else{
-			$html.= '<input type="'.$this->type.'" name="'.$this->name.'"'.($this->maxlength ? ' maxlength="'.$this->maxlength.'"' : '').' value="'.htmlspecialchars($userValue===null ? $this->value : $userValue).'"'.$onchange.'>';
+			$html.= '<input type="'.$this->type.'" name="'.$this->name.'"'.($this->maxlength ? ' maxlength="'.$this->maxlength.'"' : '').($this->placeholder ? ' placeholder="'.htmlspecialchars($this->placeholder).'"' : '').' value="'.htmlspecialchars($userValue===null ? $this->value : $userValue).'"'.$onchange.'>';
 		}
 		$html.= '</label>';
 		return parent::getHtml($html);
