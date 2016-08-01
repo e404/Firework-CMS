9af1c1aef087092bbd72fe9a628d9f3ca71f3f90
Gerald Schittenhelm <gerald@schittenhelm.at>
2016-08-01 15:00:12 +0800

    made cookies.js always loading in default head template, upgraded cookies.js version

--- app/inc/install/defaults/skin/tpl-head.php
+++ app/inc/install/defaults/skin/tpl-head.php
@@ -7,10 +7,10 @@
 		<link rel="canonical" href="[[[CANONICAL]]]">
 		<meta name="viewport" content="width=device-width, user-scalable=no">
 		<link rel="stylesheet" type="text/css" href="<? App::linkVersionedFile(App::getSkinPath().'styles.css') ?>">
+		<script type="text/javascript" src="<? App::linkVersionedFile(App::getAppDir().'lib/js.cookie.min.js') ?>"></script>
 		<script type="text/javascript" src="<?= App::linkFile(App::getAppDir().'lib/jquery-2.1.4.min.js') ?>"></script>
 		<script type="text/javascript" src="<? App::linkVersionedFile(App::getAppDir().'inc/public/app.js') ?>"></script>
 		<? if(Config::get('debug')): ?>
-			<script type="text/javascript" src="<? App::linkVersionedFile(App::getAppDir().'lib/js.cookie.min.js') ?>"></script>
 			<script type="text/javascript" src="<? App::linkVersionedFile(App::getAppDir().'inc/public/debug.js') ?>"></script>
 		<? endif; ?>
 		[[[HOOK:head]]]
