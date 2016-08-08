ea0b16b88af2805c1d6010e810abe7c8543a186b
Gerald Schittenhelm <gerald@schittenhelm.at>
2016-08-08 20:03:49 +0800

    changed search for +403.php to pages directory

--- app/inc/public/403.php
+++ app/inc/public/403.php
@@ -4,7 +4,7 @@ require_once(__DIR__.'/../global.php');
 
 header('HTTP/1.1 403 Forbidden');
 
-$skinfile = App::getSkinPath().'+403.php';
+$skinfile = Config::get('dirs', 'pages').'/+403.php';
 if(file_exists($skinfile)) {
 	include($skinfile);
 	App::halt();
