26d23b9bdd414af0fd658efe85facdacc927d021
Gerald Schittenhelm <gerald@schittenhelm.at>
2016-08-10 16:35:38 +0800

    added loop and nointeraction options to vimeo tag

--- plugins/vimeo/plugin.php
+++ plugins/vimeo/plugin.php
@@ -17,6 +17,8 @@ class VimeoVideo extends CustomHtmlTag {
 
 		$this->attr('id');
 		$this->attr('aspect', '16:9');
+		$this->attr('loop', 'false');
+		$this->attr('nointeraction', 'false');
 
 		$this->setHandler(function($atts){
 			switch($atts['aspect']) {
@@ -30,9 +32,24 @@ class VimeoVideo extends CustomHtmlTag {
 					list($w, $h) = explode(':', $atts['aspect'], 2);
 					$h = round((100/$w) * $h, 5);
 			}
+			$options = [];
+			if(in_array(strtolower($atts['loop']), ['true', 'yes', '1', 'on'])) {
+				$options[] = 'autopause=0';
+				$options[] = 'autoplay=1';
+				$options[] = 'badge=0';
+				$options[] = 'byline=0';
+				$options[] = 'loop=1';
+				$options[] = 'portrait=0';
+				$options[] = 'title=0';
+			}
 			self::$counter++;
 			$element_id = 'vimeo'.self::$counter;
-			return '<div class="video vimeo"><div class="placeholder" style="padding-top:'.$h.'%;"></div><div class="video-wrapper"><iframe id="'.$element_id.'" src="https://player.vimeo.com/video/'.$atts['id'].'?api=1&amp;player_id='.$element_id.'" frameborder="0" allowfullscreen></iframe></div></div>';
+			$html = '<div class="video vimeo"><div class="placeholder" style="padding-top:'.$h.'%;"></div><div class="video-wrapper"><iframe id="'.$element_id.'" src="https://player.vimeo.com/video/'.$atts['id'].'?api=1&amp;player_id='.$element_id.($options ? '&amp;'.implode('&amp;',$options) : '').'" frameborder="0" allowfullscreen></iframe></div>';
+			if(in_array(strtolower($atts['nointeraction']), ['true', 'yes', '1', 'on'])) {
+				$html.= '<div class="video-cover"></div>';
+			}
+			$html.= '</div>';
+			return $html;
 		});
 
 	}
