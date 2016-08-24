60628893a2c02cce1e9f8d564fb0cf2d77bdcb80
dev.websrv.eu <webmaster@websrv.eu>
2016-08-25 00:47:36 +0200

    added cors headers to webhook ajax responder

--- app/inc/public/webcron.php
+++ app/inc/public/webcron.php
@@ -3,11 +3,14 @@
 // Run this script even if the browser closes the connection
 ignore_user_abort(true);
 
+require_once('../global.php');
 ob_end_clean();
 
 // With zero content length the browser stops listening for further data
 header('Content-Encoding: none');
 header('Content-Length: 0');
+header('Access-Control-Allow-Origin: '.Config::get('env', 'protocol').self::getHost());
+header('Access-Control-Allow-Methods: GET, OPTIONS');
 
 // Flush the output to send the headers
 ob_end_flush(); // This is required for flush() to work due to a strange bug in some versions of PHP
@@ -16,8 +19,6 @@ flush();
 // ----
 // Everything below this line gets executed without the browser waiting for any output.
 // This script could take forever and leaves the user agent in a perfect state.
-
-require_once('../global.php');
 
 // Now run the webcron script
 App::webcron();
