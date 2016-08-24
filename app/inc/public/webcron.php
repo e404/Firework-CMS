c5793daae30a2198234e7e340029a85f30778a74
dev.websrv.eu <webmaster@websrv.eu>
2016-08-25 00:53:46 +0200

    fixed webcron ajax responder, added OPTIONS request handling

--- app/inc/public/webcron.php
+++ app/inc/public/webcron.php
@@ -9,12 +9,16 @@ ob_end_clean();
 // With zero content length the browser stops listening for further data
 header('Content-Encoding: none');
 header('Content-Length: 0');
-header('Access-Control-Allow-Origin: '.Config::get('env', 'protocol').self::getHost());
+header('Access-Control-Allow-Origin: '.Config::get('env', 'protocol').App::getHost());
 header('Access-Control-Allow-Methods: GET, OPTIONS');
 
 // Flush the output to send the headers
 ob_end_flush(); // This is required for flush() to work due to a strange bug in some versions of PHP
 flush();
+
+if($_SERVER['REQUEST_METHOD']==='OPTIONS') {
+	App::halt();
+}
 
 // ----
 // Everything below this line gets executed without the browser waiting for any output.
