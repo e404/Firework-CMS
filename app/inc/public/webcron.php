db462876abe330eb8527f0a6baded0f1a9445919
dev.websrv.eu <webmaster@websrv.eu>
2016-08-25 01:14:41 +0200

    fixed CORS

--- app/inc/public/webcron.php
+++ app/inc/public/webcron.php
@@ -9,16 +9,21 @@ ob_end_clean();
 // With zero content length the browser stops listening for further data
 header('Content-Encoding: none');
 header('Content-Length: 0');
-header('Access-Control-Allow-Origin: '.Config::get('env', 'protocol').App::getHost());
-header('Access-Control-Allow-Methods: GET, OPTIONS');
+
+if($_SERVER['REQUEST_METHOD']==='OPTIONS' && isset($_SERVER['HTTP_ORIGIN'])) {
+	$origin = trim(preg_replace('@^https?\://@', '', $_SERVER['HTTP_ORIGIN']), '/');
+	if($origin===App::getHost()) {
+		header('Access-Control-Allow-Origin: '.$_SERVER['HTTP_ORIGIN']);
+		header('Access-Control-Allow-Methods: GET, OPTIONS');
+	}else{
+		header('Access-Control-Allow-Origin: none');
+	}
+	App::halt();
+}
 
 // Flush the output to send the headers
 ob_end_flush(); // This is required for flush() to work due to a strange bug in some versions of PHP
 flush();
-
-if($_SERVER['REQUEST_METHOD']==='OPTIONS') {
-	App::halt();
-}
 
 // ----
 // Everything below this line gets executed without the browser waiting for any output.
