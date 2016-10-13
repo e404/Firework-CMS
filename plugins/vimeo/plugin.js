7ac99b48e189ec27f967f7ed1d33c077349cf263
dev.websrv.eu <webmaster@websrv.eu>
2016-10-13 08:05:28 +0200

    added subtitle flag to <vimeo> plugin

--- plugins/vimeo/plugin.js
+++ plugins/vimeo/plugin.js
@@ -5,6 +5,12 @@ app.plugins.vimeo = {
 		if(!app.plugins.vimeo.iframes.length) return;
 		app.plugins.vimeo.iframes.load(function(){
 			$(this).closest('.video').addClass('loaded');
+			var subtitles = $(this).data('subtitles');
+			if(subtitles) {
+				this.contentWindow.postMessage('{"method":"enableTextTrack","value":{"language":"'+subtitles+'","kind":"subtitles"}}', '*');
+			}else{
+				this.contentWindow.postMessage('{"method":"disableTextTrack","value":{"kind":"subtitles"}}', '*');
+			}
 		});
 	}
 };
