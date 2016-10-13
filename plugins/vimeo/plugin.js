c957f979b16e049cf16ae3236b5d03f73660b90f
dev.websrv.eu <webmaster@websrv.eu>
2016-10-13 14:19:33 +0200

    setting subtitles in 1s intervals to force them

--- plugins/vimeo/plugin.js
+++ plugins/vimeo/plugin.js
@@ -8,16 +8,13 @@ app.plugins.vimeo = {
 				var iframe = $(this);
 				iframe.closest('.video').addClass('loaded');
 				var subtitles = iframe.data('subtitles');
-				var ensureSubtitles = function(){
+				setInterval(function(){
 					if(subtitles) {
 						iframe[0].contentWindow.postMessage('{"method":"enableTextTrack","value":{"language":"'+subtitles+'","kind":"subtitles"}}', '*');
 					}else{
 						iframe[0].contentWindow.postMessage('{"method":"disableTextTrack","value":{"kind":"subtitles"}}', '*');
 					}
-				};
-				setTimeout(ensureSubtitles, 100);
-				setTimeout(ensureSubtitles, 1000);
-				setTimeout(ensureSubtitles, 2000);
+				}, 1000);
 			});
 		});
 	}
