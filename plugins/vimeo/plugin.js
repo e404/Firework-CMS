96db24aa55123cbbc482d700745a1f5446c89a53
dev.websrv.eu <webmaster@websrv.eu>
2016-10-13 08:39:52 +0200

    fixed subtitles

--- plugins/vimeo/plugin.js
+++ plugins/vimeo/plugin.js
@@ -8,13 +8,16 @@ app.plugins.vimeo = {
 				var iframe = $(this);
 				iframe.closest('.video').addClass('loaded');
 				var subtitles = iframe.data('subtitles');
-				setTimeout(function(){
+				var ensureSubtitles = function(){
 					if(subtitles) {
 						iframe[0].contentWindow.postMessage('{"method":"enableTextTrack","value":{"language":"'+subtitles+'","kind":"subtitles"}}', '*');
 					}else{
 						iframe[0].contentWindow.postMessage('{"method":"disableTextTrack","value":{"kind":"subtitles"}}', '*');
 					}
-				}, 2000);
+				};
+				setTimeout(ensureSubtitles, 100);
+				setTimeout(ensureSubtitles, 1000);
+				setTimeout(ensureSubtitles, 2000);
 			});
 		});
 	}
