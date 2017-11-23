1bdedcfaf294131e18ae3de82769e004e92bb55f
dev.websrv.eu <webmaster@websrv.eu>
2017-11-23 14:32:07 +0100

    (auto) Regenerating docs

--- index.php
+++ index.php
@@ -41,11 +41,7 @@ if($forbidden) {
 	include(App::getAppDir().'inc/public/403.php');
 	die();
 }elseif(isset($_SERVER['REDIRECT_IS_AJAX_REQUEST'])) {
-	header('Expires: Sat, 26 Jul 1997 05:00:00 GMT');
-	header('Last-Modified: ' . gmdate( 'D, d M Y H:i:s') . ' GMT');
-	header('Cache-Control: no-store, no-cache, must-revalidate');
-	header('Cache-Control: post-check=0, pre-check=0', false);
-	header('Pragma: no-cache');
+	App::neverCache();
 	App::renderajax();
 }elseif(isset($_SERVER['REDIRECT_IS_LINK_REQUEST'])) {
 	LinkTracker::action();
